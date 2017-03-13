094ceef1473ff234f58dc316385b219328161180
Simon Gonzalez <sgonzalez@galpaservices.com>
2017-03-13 10:15:12 -0400

    Activity Busqueda Captura de la búsqueda

--- app/src/main/java/com/exam/simongonzalez/umovienow/SearchResultsActivity.java
+++ app/src/main/java/com/exam/simongonzalez/umovienow/SearchResultsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class SearchResultsActivity extends AppCompatActivity {
 
@@ -14,9 +15,6 @@ public class SearchResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         handleIntent(getIntent());
     }
@@ -30,7 +28,11 @@ public class SearchResultsActivity extends AppCompatActivity {
 
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            this.toolbar.setTitle(query);
+
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle(query);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
 
